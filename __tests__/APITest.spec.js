bee43ffd3ca9be594cafad8f0c6ff031cf65ba3b
WillianFarias <williansousafarias@gmail.com>
2020-10-02 16:33:16 -0300

    Ajustando para json-server

--- __tests__/APITest.spec.js
+++ __tests__/APITest.spec.js
@@ -1,4 +1,4 @@
-const { getUsers } = require("../src/common/usersAPI");
+/*const { getUsers } = require("../src/common/usersAPI");
 
 beforeAll(() => {
   require("whatwg-fetch");
@@ -38,5 +38,27 @@ describe("Users API", () => {
     //restaurando versao real do fetch
     window.fetch.mockRestore();
     
+  });
+});*/
+
+//json-server
+const { getUsers } = require("../src/common/usersAPI");
+
+beforeAll(() => {
+  require("whatwg-fetch");
+});
+
+describe("Users API", () => {
+  test("it returns an array of users", async () => {
+    const expected = [
+      { name: "Jonas", surname: "T." },
+      { name: "Chris", surname: "B." },
+      { name: "Juliana", surname: "Crain" },
+      { name: "Caty", surname: "B." }
+    ];
+
+    const json = await getUsers("http://localhost:3000/users");
+
+    expect(json).toMatchObject(expected);
   });
 });
