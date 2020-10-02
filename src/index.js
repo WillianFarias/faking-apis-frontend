b3f7eb5169de919e1561cd9b569f17a0920900cb
WillianFarias <williansousafarias@gmail.com>
2020-10-02 09:29:03 -0300

    criando end point falso com objeto Server do miragejs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-/*
-START HERE!
- */
+import { Server } from "miragejs";
+
+//interceptando a chamada para api/users/ com miragejs usando um objto Server
+//configurado com um endpoint falso. *poderia estar em outro lugar
+new Server({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/users/", () => {
+      return [
+        { name: "Angy", surname: "T." },
+        { name: "Chris", surname: "B." },
+        { name: "Juliana", surname: "Crain" }
+      ];
+    });
+  }
+});
+
+
+//obter dados ao clicar no botao
+const button = document.getElementById("fetch-btn");
+
+button.addEventListener("click", function() {
+  fetch("/api/users")
+  .then(response => {
+    if (!response.ok) throw Error(response.statusText);
+    return response.json();
+  })
+  .then(json => buildList(json));
+});
+
+//lista de usuário
+function buildList(data) {
+  console.log(data);
+}
+
